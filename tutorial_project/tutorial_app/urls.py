d51672fe8822957667d0d9fa40e892a45e905727
dannyxboi <danielorrego02@gmail.com>
2016-10-20 20:55:54 -0400

    jhbhbhb

--- tutorial_project/tutorial_app/urls.py
+++ tutorial_project/tutorial_app/urls.py
@@ -4,4 +4,4 @@ from tutorial_app import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'))
-    
+    url(r'^about/' views.about, name='about'))
--- tutorial_project/tutorial_project/urls.py
+++ tutorial_project/tutorial_project/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf.urls.static import static​​​​​​​
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,4 +9,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('tutorial_app.urls')), # ADD THIS NEW TUPLE!
-)
+)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
